065c186e0975350d2ba707a46f8a679da93cc4d6
yururiwork <yururiwork@gmail.com>
2024-07-28 15:30:35 +0900

    実装も警告が出るか確認

--- github_workflow_test/lib/main.dart
+++ github_workflow_test/lib/main.dart
@@ -56,6 +56,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  var _unUsedValue = 0;
 
   void _incrementCounter() {
     setState(() {
